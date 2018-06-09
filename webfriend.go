53dcd2e0026a627a39c7db9be57887324ee98f7c
Gary Hetzel <garyhetzel@gmail.com>
2018-06-09 08:33:00 -0400

    version bump

--- webfriend.go
+++ webfriend.go
@@ -2,5 +2,5 @@ package webfriend
 
 //go:generate esc -o server/static.go -pkg server -modtime 1500000000 -prefix ui ui
 
-const Version = `0.9.0`
+const Version = `0.9.1`
 const Slogan = `Your friendly friend in web browser automation.`
