9ea62786f3022c0ef79c18210eeb9776169f37e1
Gary Hetzel <garyhetzel@gmail.com>
2018-07-18 14:38:46 -0400

    Update deps

--- webfriend.go
+++ webfriend.go
@@ -3,5 +3,5 @@ package webfriend
 //go:generate ./bin/webfriend-autodoc ui/documentation.gob
 //go:generate esc -o server/static.go -pkg server -modtime 1500000000 -prefix ui ui
 
-const Version = `0.9.6`
+const Version = `0.9.7`
 const Slogan = `Your friendly friend in web browser automation.`
