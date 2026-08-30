766a20bdab6c1ed8e43baba724a4af298231dfd8
Gary Hetzel <its@gary.cool>
2026-08-30 18:57:05 -0400

    version bump

--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package webfriend
 
 //go:generate ./bin/webfriend-autodoc server/ui/documentation.json
 
-const Version = `1.0.0`
+const Version = `1.0.1`
 const Slogan = `Your friendly friend in web browser automation.`
